35f90e2eae615bf523c4b9b035ac2dfaabde2bce
ASHUTOSH-309 <125751575+ASHUTOSH-309@users.noreply.github.com>
2023-09-25 22:18:43 +0530

    added an user in the users array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-const USERS = [];
+const USERS = [{name:"ashutosh",password:123}];
 
 const QUESTIONS = [{
     title: "Two states",
